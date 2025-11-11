d6262981a87b4af97c61f81669598bbc564660df
DarielMariano <dm23-1711@unphu.edu.do>
2025-11-11 13:22:03 -0600

    Add files via upload
    modo negro habilitado

--- js/java.js
+++ js/java.js
@@ -1,4 +1,26 @@
-const toggle = document.getElementById('darkToggle');
-toggle.addEventListener('click', () => {
-    document.body.classList.toggle('dark-mode');
+const temaOscuro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
+    localStorage.setItem("darkmode", "true");
+}
+
+const temaClaro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "light");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
+    localStorage.setItem("darkmode", "false");
+}
+
+const cambiarTema = () => {
+    const body = document.querySelector("body");
+    const temaActual = body.getAttribute("data-bs-theme");
+    temaActual === "light" ? temaOscuro() : temaClaro();
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+    if (localStorage.getItem("darkmode") === "true") {
+        temaOscuro();
+    } else {
+        temaClaro();
+    }
 });
